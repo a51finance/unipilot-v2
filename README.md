a90a847287d558ab23f559be3a4d1870a812ee3f
mudassir45 <mudassirali36@gmail.com>
2021-12-23 14:57:59 +0500

    feat: updated script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-#Unipilot - Universal automated liquidity optimization#
+# Unipilot - Universal automated liquidity optimization
+
+This repository contains the core smart contracts for the Unipilot Protocol.
+
+## Documentation
+
+The full documentation can be found at: https://unipilot.gitbook.io/unipilot/
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,10 @@
 import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
 import "./tasks/accounts";
-import "./tasks/deploy";
 
 import { resolve } from "path";
 
@@ -15,6 +15,7 @@ import { NetworkUserConfig } from "hardhat/types";
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
 const chainIds = {
+  ganache: 1337,
   goerli: 5,
   hardhat: 31337,
   kovan: 42,
@@ -24,26 +25,25 @@ const chainIds = {
 };
 
 // Ensure that we have all the environment variables we need.
-const mnemonic: string | undefined = process.env.MNEMONIC;
+const mnemonic = process.env.MNEMONIC;
 if (!mnemonic) {
   throw new Error("Please set your MNEMONIC in a .env file");
 }
 
-const infuraApiKey: string | undefined = process.env.INFURA_API_KEY;
+const infuraApiKey = process.env.INFURA_API_KEY;
 if (!infuraApiKey) {
   throw new Error("Please set your INFURA_API_KEY in a .env file");
 }
 
-function getChainConfig(network: keyof typeof chainIds): NetworkUserConfig {
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY;
+
+function createTestnetConfig(network: keyof typeof chainIds): NetworkUserConfig {
   const url: string = "https://" + network + ".infura.io/v3/" + infuraApiKey;
   return {
-    accounts: {
-      count: 10,
-      mnemonic,
-      path: "m/44'/60'/0'/0",
-    },
+    accounts: [`${process.env.PK1}`, `${process.env.PK2}`],
     chainId: chainIds[network],
     url,
+    gasPrice: 120000000000,
   };
 }
 
@@ -62,10 +62,15 @@ const config: HardhatUserConfig = {
       },
       chainId: chainIds.hardhat,
     },
-    goerli: getChainConfig("goerli"),
-    kovan: getChainConfig("kovan"),
-    rinkeby: getChainConfig("rinkeby"),
-    ropsten: getChainConfig("ropsten"),
+
+    goerli: createTestnetConfig("goerli"),
+    kovan: createTestnetConfig("kovan"),
+    rinkeby: createTestnetConfig("rinkeby"),
+    ropsten: createTestnetConfig("ropsten"),
+    mainnet: createTestnetConfig("mainnet"),
+  },
+  mocha: {
+    timeout: 50000,
   },
   paths: {
     artifacts: "./artifacts",
@@ -74,23 +79,34 @@ const config: HardhatUserConfig = {
     tests: "./test",
   },
   solidity: {
-    version: "0.8.9",
-    settings: {
-      metadata: {
-        
-        bytecodeHash: "none",
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          metadata: {
+            // Not including the metadata hash
+            // https://github.com/paulrberg/solidity-template/issues/31
+            bytecodeHash: "none",
+          },
+          // You should disable the optimizer when debugging
+          // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+          optimizer: {
+            enabled: true,
+            runs: 800,
+          },
+        },
       },
-      // Disable the optimizer when debugging
-      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
-      optimizer: {
-        enabled: true,
-        runs: 800,
+      {
+        version: "0.7.5",
       },
-    },
+    ],
   },
   typechain: {
-    outDir: "src/types",
+    outDir: "typechain",
     target: "ethers-v5",
+  },
+  etherscan: {
+    apiKey: etherscanApiKey,
   },
 };
 
