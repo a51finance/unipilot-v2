325198d90194908d523bde6e964ccdd35d9d0fc4
mudassir45 <mudassirali36@gmail.com>
2021-12-30 19:29:26 +0500

    feat: liquidity handling methods on UniswapV3

---
+++ contracts/libraries/UniswapPoolActions.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.5.0;
+
+import "./SafeCast.sol";
+import "./UniswapLiquidityManagment.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+
+/// @title Liquidity and ticks functions
+/// @notice Provides functions for computing liquidity and ticks for token amounts and prices
+library UniswapPoolActions {
+    using UniswapLiquidityManagment for IUniswapV3Pool;
+    using SafeCast for uint256;
+
+    function updatePosition(
+        IUniswapV3Pool pool,
+        int24 tickLower,
+        int24 tickUpper
+    ) internal {
+        (uint128 liquidity, , ) = pool.getPositionLiquidity(
+            tickLower,
+            tickUpper
+        );
+
+        if (liquidity > 0) {
+            pool.burn(tickLower, tickUpper, 0);
+        }
+    }
+
+    function burnLiquidity(
+        IUniswapV3Pool pool,
+        int24 tickLower,
+        int24 tickUpper,
+        address recipient
+    ) internal returns (uint256 amount0, uint256 amount1) {
+        (uint128 liquidity, , ) = pool.getPositionLiquidity(
+            tickLower,
+            tickUpper
+        );
+
+        (amount0, amount1) = pool.burn(tickLower, tickUpper, liquidity);
+
+        if (amount0 > 0 || amount1 > 0) {
+            (amount0, amount0) = pool.collect(
+                recipient,
+                tickLower,
+                tickUpper,
+                amount0.toUint128(),
+                amount1.toUint128()
+            );
+        }
+    }
+
+    function burnUserLiquidity(
+        IUniswapV3Pool pool,
+        int24 tickLower,
+        int24 tickUpper,
+        uint256 userSharePercentage,
+        address recipient
+    ) internal returns (uint256 amount0, uint256 amount1) {
+        (uint128 liquidity, , ) = pool.getPositionLiquidity(
+            tickLower,
+            tickUpper
+        );
+
+        uint256 liquidityRemoved = FullMath.mulDiv(
+            uint256(liquidity),
+            userSharePercentage,
+            1e18
+        );
+
+        (amount0, amount1) = pool.burn(
+            tickLower,
+            tickUpper,
+            liquidityRemoved.toUint128()
+        );
+
+        if (amount0 > 0 || amount1 > 0) {
+            (amount0, amount0) = pool.collect(
+                recipient,
+                tickLower,
+                tickUpper,
+                amount0.toUint128(),
+                amount1.toUint128()
+            );
+        }
+    }
+}
