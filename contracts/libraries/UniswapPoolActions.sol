f0ac6ec183fb1fe822a6cf1dcfb610c2d41d54d0
mudassir45 <mudassirali36@gmail.com>
2022-01-04 19:12:31 +0500

    fix: updated burnLiquidity method

--- contracts/libraries/UniswapPoolActions.sol
+++ contracts/libraries/UniswapPoolActions.sol
@@ -8,6 +8,7 @@ import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 /// @title Liquidity and ticks functions
 /// @notice Provides functions for computing liquidity and ticks for token amounts and prices
 library UniswapPoolActions {
+    using LowGasSafeMath for uint256;
     using SafeCastExtended for uint256;
     using UniswapLiquidityManagement for IUniswapV3Pool;
 
@@ -31,23 +32,29 @@ library UniswapPoolActions {
         int24 tickLower,
         int24 tickUpper,
         address recipient
-    ) internal returns (uint256 amount0, uint256 amount1) {
+    ) internal returns (uint256 fees0, uint256 fees1) {
         (uint128 liquidity, , ) = pool.getPositionLiquidity(
             tickLower,
             tickUpper
         );
 
         if (liquidity > 0) {
-            (amount0, amount1) = pool.burn(tickLower, tickUpper, liquidity);
+            (uint256 amount0, uint256 amount1) = pool.burn(
+                tickLower,
+                tickUpper,
+                liquidity
+            );
 
             if (amount0 > 0 || amount1 > 0) {
-                (amount0, amount0) = pool.collect(
+                (uint256 collect0, uint256 collect1) = pool.collect(
                     recipient,
                     tickLower,
                     tickUpper,
                     amount0.toUint128(),
                     amount1.toUint128()
                 );
+
+                (fees0, fees1) = (collect0.sub(amount0), collect1.sub(amount1));
             }
         }
     }
