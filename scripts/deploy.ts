7fc39abddb81f8ee2667f30c49daf8a6e1906795
mudassir45 <mudassirali36@gmail.com>
2022-01-07 20:43:27 +0500

    fix: updated scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -10,10 +10,13 @@ import { resolve } from "path";
 
 import UnipilotFactoryArtifacts from "../artifacts/contracts/UnipilotFactory.sol/UnipilotFactory.json";
 import UnipilotVaultArtifacts from "../artifacts/contracts/UnipilotVault.sol/UnipilotVault.json";
+import UnipilotStrategyArtifacts from "../artifacts/contracts/UnipilotStrategy.sol/UnipilotStrategy.json";
+import ERC20Artifact from "../artifacts/contracts/test/ERC20.sol/ERC20.json";
 
 dotenvConfig({ path: resolve(__dirname, "../.env") });
 
 const web3 = new Web3(process.env.INFURA_URL!);
+const UnipilotStrategy = "0x3DB63a880FFaED0aBfA40496366fcc383256ecCA";
 
 const contractUnipilotFactory = new web3.eth.Contract(
   UnipilotFactoryArtifacts.abi as AbiItem[],
@@ -24,6 +27,12 @@ let wallet: Signer;
 let wallet2: Signer;
 let privateKey: any = process.env.PK1;
 let privateKey2: any = process.env.PK2;
+
+const TKN1_TOKEN_ADDRESS = "0x6df730f6e52c57be77db98a65116d2c38ec2be2b"; // DST
+const TKN2_TOKEN_ADDRESS = "0xdf98809bbaee8d72ba88a80bc99308e30e04e4ab";
+const TTC33 = "0xB6118140b5Ad8A1449D1FeF850dA49eE4677D77A";
+const TTC99 = "0xEc025986c17b4476dB7D3F3A6065f757C56f9CAe";
+const pool = "0x2c028E572c5e8708aA6264a0E87396e36E477DD0"; // TTCC99 & TKN5
 
 async function updateStateVariables(): Promise<void> {
   const _wallet = await ethers.getSigners();
@@ -46,11 +55,29 @@ const deployUnipilotFactory = async () => {
   const UnipilotFactoryContract = await UnipilotFactory.deploy(
     "0x1F98431c8aD98523631AE4a59f267346ea31F984",
     walletAddress,
-    walletAddress,
+    UnipilotStrategy,
   );
   console.log(
     "UnipilotFactory deployed to:",
     UnipilotFactoryContract.address,
+    "from account,",
+    walletAddress,
+  );
+};
+
+const deployUnipilotStrategy = async () => {
+  const UnipilotFactory = new ethers.ContractFactory(
+    UnipilotStrategyArtifacts.abi,
+    UnipilotStrategyArtifacts.bytecode,
+    wallet,
+  );
+
+  const walletAddress = await wallet.getAddress();
+  const UnipilotStrategy = await UnipilotFactory.deploy(walletAddress);
+
+  console.log(
+    "UnipilotStrategy deployed to:",
+    UnipilotStrategy.address,
     "from account,",
     walletAddress,
   );
@@ -68,6 +95,29 @@ const deployVault = async (
   );
 };
 
+async function getERC20Approval(
+  tokenAddress: string,
+  spenderAddress: string,
+): Promise<void> {
+  const tokenContract = new ethers.ContractFactory(
+    ERC20Artifact.abi,
+    ERC20Artifact.bytecode,
+    wallet,
+  );
+
+  const tokenContractInstance = tokenContract.attach(tokenAddress);
+
+  const approval = await tokenContractInstance.approve(
+    spenderAddress,
+    MaxUint256,
+    {
+      gasLimit: "10000000",
+      gasPrice: "13535202943",
+    },
+  );
+  console.log("getERC20Approval -> ", approval.hash);
+}
+
 const checkGovernance = async (contractAddress: string) => {
   const uniPilotFactoryContract = new ethers.ContractFactory(
     UnipilotFactoryArtifacts.abi,
@@ -83,7 +133,13 @@ const checkGovernance = async (contractAddress: string) => {
 const main = async () => {
   await updateStateVariables();
   await updateStateVariables2();
-  await deployUnipilotFactory();
+  // await deployUnipilotFactory();
+  // await deployUnipilotStrategy();
+  await getERC20Approval(TTC99, "0x089d97D30934125691544c81a0e158F84dDb2190");
+  await getERC20Approval(
+    TKN2_TOKEN_ADDRESS,
+    "0x089d97D30934125691544c81a0e158F84dDb2190",
+  );
   // await checkGovernance("0xcE8f9628aD97D45ee6B7088ccd316D11B37cce71");
 };
 
--- tasks/accounts.ts
+++ tasks/accounts.ts
@@ -1,10 +1,71 @@
-import { Signer } from "@ethersproject/abstract-signer";
+import { deployContract } from "./utils";
+import { constants, Wallet } from "ethers";
+import { formatEther, parseEther } from "ethers/lib/utils";
+import { ethers, network } from "hardhat";
 import { task } from "hardhat/config";
+import { Bytes, formatUnits, parseUnits, AbiCoder } from "ethers/lib/utils";
 
-task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
-  const accounts: Signer[] = await hre.ethers.getSigners();
+task("deploy-unipilot", "Deploy all unipilot contracts")
+  .addParam("pool", "UniswapV3 Pool")
+  .addParam("governance", "governer address")
+  .addParam("name", "Unipilot LP Token name")
+  .addParam("symbol", "Unipilot LP Token symbol")
+  .setAction(async (cliArgs, { ethers, run, network }) => {
+    await run("compile");
 
-  for (const account of accounts) {
-    console.log(await account.getAddress());
-  }
-});
+    const signer = (await ethers.getSigners())[0];
+    console.log("Signer");
+    console.log("  at", signer.address);
+    console.log("  ETH", formatEther(await signer.getBalance()));
+
+    const args = {
+      pool: cliArgs.pool,
+      router: "0x0000000000000000000000000000000000000000",
+      strategy: "0x3DB63a880FFaED0aBfA40496366fcc383256ecCA",
+      governance: cliArgs.governance,
+      unipilotFactory: "0x8dA851D05a6122d28294D624F03Af533aFeF01da",
+      name: cliArgs.name,
+      symbol: cliArgs.symbol,
+    };
+
+    console.log("Network");
+    console.log("   ", network.name);
+    console.log("Task Args");
+    console.log(args);
+
+    const unipilotVault = await deployContract(
+      "UnipilotVault",
+      await ethers.getContractFactory("UnipilotVault"),
+      signer,
+      [
+        args.pool,
+        args.router,
+        args.strategy,
+        args.governance,
+        args.unipilotFactory,
+        args.name,
+        args.symbol,
+      ],
+    );
+
+    await unipilotVault.deployTransaction.wait(5);
+
+    delay(60000);
+
+    await run("verify:verify", {
+      address: unipilotVault.address,
+      constructorArguments: [
+        args.pool,
+        args.router,
+        args.strategy,
+        args.governance,
+        args.unipilotFactory,
+        args.name,
+        args.symbol,
+      ],
+    });
+  });
+
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
