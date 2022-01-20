2e038626ac67da4d720da66241613b6f652283ec
Mubashir-ali-baig <mubashirbaig98@gmail.com>
2022-01-20 14:17:30 +0500

    build: passive modular

--- test/unipilot.ts
+++ test/unipilot.ts
@@ -2,12 +2,17 @@ import { use } from "chai";
 import { solidity } from "ethereum-waffle";
 
 import { shouldBehaveLikeDepositActive } from "./DepositActive/depositActive.behavior";
+import { shouldBehaveLikeDepositPassive } from "./DepositPassive/depositPassive.behavior";
 import { shouldBehaveLikeUnipilotFactory } from "./UnipilotFactoryFunctions/UnipilotFactory.behavior";
 
 use(solidity);
 
 describe("Invokes Deposit Active Tests", async () => {
   await shouldBehaveLikeDepositActive();
+});
+
+describe("Invokes Deposit Passive Tests", async () => {
+  await shouldBehaveLikeDepositPassive();
 });
 
 describe("Invokes Unipilot Factory Tests", async () => {
