578dd9129d2c8bee0d7dec5d1f0b5c3a6ca37b80
faheelsattar <faheelsattar@yahoo.com>
2022-01-25 15:07:41 +0500

    build: withdraw testcases merged

--- test/unipilot.ts
+++ test/unipilot.ts
@@ -20,6 +20,6 @@ describe("Invokes Rebalance Active Tests", async () => {
 //   await shouldBehaveLikeUnipilotFactory();
 // })
 
-describe("Withdraw Liquidity", async () => {
-  await shouldBehaveLikeWithdraw();
-});
+// describe("Withdraw Liquidity", async () => {
+//   await shouldBehaveLikeWithdraw();
+// });
