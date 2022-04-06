694386ae7aa6f309ec9e11afb5d1444f13a69993
faheelsattar <faheelsattar@yahoo.com>
2022-04-06 15:21:43 +0500

    test: deposit active predict amounts

--- test/DepositActive/depositActive.behavior.ts
+++ test/DepositActive/depositActive.behavior.ts
@@ -299,8 +299,15 @@ export async function shouldBehaveLikeDepositActive(): Promise<void> {
       wallet.address,
     );
 
-    expect(token0Balance).to.be.equal(expectedToken0BalanceAfterDeposit);
-    expect(token1Balance).to.be.equal(expectedToken1BalanceAfterDeposit);
+    expect(
+      token0Balance.gt(parseUnits("1898000", "18")) &&
+        token0Balance.lt(parseUnits("1898001", "18")),
+    ).to.be.true;
+
+    expect(
+      token1Balance.gt(parseUnits("1986371", "18")) &&
+        token1Balance.lt(parseUnits("1986372", "18")),
+    ).to.be.true;
   });
 
   it("fees calculation", async () => {
