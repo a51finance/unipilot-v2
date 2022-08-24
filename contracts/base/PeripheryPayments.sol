5a0375e9fea1cfa50ec83c7541a4d17a1f79d666
mudassir45 <mudassirali36@gmail.com>
2022-08-24 16:59:07 +0500

    fix: update WMATIC address

--- contracts/base/PeripheryPayments.sol
+++ contracts/base/PeripheryPayments.sol
@@ -5,7 +5,7 @@ import "../interfaces/external/IWETH9.sol";
 import "../libraries/TransferHelper.sol";
 
 abstract contract PeripheryPayments {
-    address internal constant WETH = 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889;
+    address internal constant WETH = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
 
     receive() external payable {}
 
